92a01fa2796ca803e5f9172e8a6e18c9fa81a2ac
Jorge Arindia <jorgearindia@gmail.com>
2023-01-25 16:50:46 -0500

    add test move test

--- Tests/snakesLaddersTests/snakesLaddersTests.swift
+++ Tests/snakesLaddersTests/snakesLaddersTests.swift
@@ -17,4 +17,14 @@ final class tokenCanMoveAcrossTheBoardTests: XCTestCase {
     game.token1.move(spaces: 3)
     XCTAssertEqual(game.token1.position, 4, "Token should be on square 4")
   }
+  
+  // UAT3
+  func testMoveToken3AndThen4Spaces() throws {
+    let game = SnakesLadders()
+    XCTAssertEqual(game.token1.position, 1, "Token should be on square 1")
+    game.token1.move(spaces: 3)
+    XCTAssertEqual(game.token1.position, 4, "Token should be on square 4")
+    game.token1.move(spaces: 4)
+    XCTAssertEqual(game.token1.position, 8, "Token should be on square 8")
+  }
 }
