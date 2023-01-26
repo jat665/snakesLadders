48959d04bfb5faed60a9433efa9d3b26f8197642
Jorge Arindia <jorgearindia@gmail.com>
2023-01-25 19:18:29 -0500

    add move token the roll dice result

--- Sources/snakesLadders/snakesLadders.swift
+++ Sources/snakesLadders/snakesLadders.swift
@@ -1,14 +1,22 @@
 @main
 public struct SnakesLadders {
   let token1 = Token()
-  let dice = Die()
+  let dice: Dice
   
+  init(dice: Dice = Dice()) {
+    self.dice = dice
+  }
+  
+  func rollTheDice() {
+    token1.move(spaces: dice.roll())
+  }
+
   public static func main() {}
 }
 
 class Token {
   var position = Board.initialPosition
-    
+      
   @discardableResult
   func move(spaces: Int) -> Bool {
     if ((position + spaces) > Board.finalPosition) {
@@ -23,12 +31,12 @@ class Token {
   }
 }
 
-class Die {
+class Dice {
   private static let start = 1
   private static let end = 6
   
   func roll() -> Int {
-    return Int.random(in: Die.start...Die.end)
+    return Int.random(in: Dice.start...Dice.end)
   }
 }
 
--- Tests/snakesLaddersTests/MovesByDiceRolls.swift
+++ Tests/snakesLaddersTests/MovesByDiceRolls.swift
@@ -12,12 +12,33 @@ import XCTest
 final class MovesByDiceRolls: XCTestCase {
   
   // UAT1
-  func testPlayerRollTheDie() throws {
-    let game = SnakesLadders()
+  func testPlayerRollTheDice() throws {
+    let dice = Dice()
     for _ in 1...100 {
-      let dieResult = game.dice.roll()
-      XCTAssertGreaterThanOrEqual(dieResult, 1, "the die result should be greater than or equal to 1")
-      XCTAssertLessThanOrEqual(dieResult, 6, "the die result should be less than or equal to 6")
+      let diceResult = dice.roll()
+      XCTAssertGreaterThanOrEqual(diceResult, 1, "dice result should be greater than or equal to 1")
+      XCTAssertLessThanOrEqual(diceResult, 6, "dice result should be less than or equal to 6")
     }
+  }
+  
+  // UAT2
+  func testPlayerMoveTokenDiceResult() throws {
+    let game = SnakesLadders(dice: FakeDice(result: 4))
+    XCTAssertEqual(game.token1.position, 1, "Token should be on square 1")
+    game.rollTheDice()
+    XCTAssertEqual(game.token1.position, 5, "Token should be on square 5")
+
+  }
+}
+
+class FakeDice: Dice {
+  let result: Int
+  
+  init(result: Int) {
+    self.result = result
+  }
+  
+  override func roll() -> Int {
+    return result
   }
 }
